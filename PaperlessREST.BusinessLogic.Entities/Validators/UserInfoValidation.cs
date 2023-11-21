bfffaa68f0851e6759bc2a7ab8e66a7e65d4e2ed
Erdem Sevilmis <51912221+Erdem-Sevilmis@users.noreply.github.com>
2023-11-21 23:42:17 +0100

    Add Validation for each BuisnessLogic Entitie

---
+++ PaperlessREST.BusinessLogic.Entities/Validators/UserInfoValidation.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperlessREST.BusinessLogic.Entities.Validators
+{
+    public class UserInfoValidation : AbstractValidator<UserInfo>
+    {
+        public UserInfoValidation() {
+            RuleFor(userInfo => userInfo.Username).NotNull().NotEmpty();
+            RuleFor(userInfo => userInfo.Password).NotNull().NotEmpty();
+        }
+    }
+}
