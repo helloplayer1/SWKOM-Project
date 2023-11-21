2f524e5690ca0293b705392eacfa71f1c493febf
Erdem Sevilmis <51912221+Erdem-Sevilmis@users.noreply.github.com>
2023-11-21 23:43:33 +0100

    Add Validation for each BuisnessLogic Entitie (#1)
    * Update docker-compose.yml
    
    * fixed compose file
    
    * created new projects, added entitites
    
    * Add Validation for each BuisnessLogic Entitie
    
    ---------
    
    Co-authored-by: captain1337 <39183415+danielcaptain@users.noreply.github.com>
    Co-authored-by: Julian Haderlein <nolimitsmail1@gmail.com>

---
+++ PaperlessREST.BusinessLogic.Entities/Validators/DocumentValidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace PaperlessREST.BusinessLogic.Entities.Validators
+{
+    public class DocumentValidation: AbstractValidator<Document>
+    {
+        public DocumentValidation() {
+            RuleFor(document => document.Id).NotNull().NotEmpty();
+            RuleFor(document => document.Correspondent).NotNull().NotEmpty();
+            RuleFor(document => document.DocumentType).NotNull().NotEmpty();
+            RuleFor(document => document.StoragePath).NotNull().NotEmpty();
+            RuleFor(document => document.Title).NotNull().NotEmpty();
+            RuleFor(document => document.Content).NotNull().NotEmpty();
+            RuleFor(document => document.Tags).NotNull().NotEmpty();
+            RuleFor(document => document.Created).NotNull().NotEmpty();
+            RuleFor(document => document.CreatedDate).NotNull().NotEmpty();
+            RuleFor(document => document.Modified).NotNull().NotEmpty();
+            RuleFor(document => document.Added).NotNull().NotEmpty();
+            RuleFor(document => document.ArchiveSerialNumber).NotNull().NotEmpty();
+            RuleFor(document => document.OriginalFileName).NotNull().NotEmpty();
+            RuleFor(document => document.ArchivedFileName).NotNull().NotEmpty();
+        }
+    }
+}
