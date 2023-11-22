b184b2a976fcbb33e58b4d4021955da98604748a
Erdem Sevilmis <51912221+Erdem-Sevilmis@users.noreply.github.com>
2023-11-22 23:34:48 +0100

    Start  implementation for Enitiy Framework

---
+++ src/PaperlessREST.DataAccess.Sql/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaperlessREST.BusinessLogic.Entities;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace PaperlessREST.DataAccess.Sql
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            /*
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            optionsBuilder.UseNpgsql(Configuration.GetConnectionString("BloggingDatabase"));
+            */
+        }
+    }
+}
