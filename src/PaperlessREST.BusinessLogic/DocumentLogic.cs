a43cd8d27a64809b784fcd59eb7681bdc076c8c3
Julian Haderlein <nolimitsmail1@gmail.com>
2023-12-01 00:15:47 +0100

    added document repository

--- src/PaperlessREST.BusinessLogic/DocumentLogic.cs
+++ src/PaperlessREST.BusinessLogic/DocumentLogic.cs
@@ -1,6 +1,9 @@
 ﻿using PaperlessREST.BusinessLogic.Entities;
 using PaperlessREST.BusinessLogic.Interfaces;
 using PaperlessREST.ServiceAgents.Interfaces;
+using AutoMapper;
+using PaperlessREST.DataAccess.Entities;
+using PaperlessREST.DataAccess.Interfaces;
 
 namespace PaperlessREST.BusinessLogic
 {
@@ -8,17 +11,25 @@ namespace PaperlessREST.BusinessLogic
     {
         //private IDocumentRepository
         private IOCRService _OCRService;
-        public DocumentLogic(IOCRService oCRService)
+        private IMapper _mapper;
+        private IDocumentRepository _documentRepository;
+        public DocumentLogic(IOCRService oCRService, IMapper mapper, IDocumentRepository documentRepository)
         {
             _OCRService = oCRService;
+            _mapper = mapper;
+            _documentRepository = documentRepository;
         }
         public void IndexDocument(Document document, Stream pdfStream)
         {
             document.Content = _OCRService.PerformORC(pdfStream);
-            
+
             //save File to disk
 
-            //save Document to db
+            //write path to document dao
+
+            DocumentDao documentDao = _mapper.Map<Document, DocumentDao>(document);
+
+            _documentRepository.Add(documentDao);
         }
     }
 }
